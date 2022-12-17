be13e5df60896784efcb9509b1609daec125e324
unknown <miguelsant2001@gmail.com>
2022-12-17 12:47:34 -0500

    please

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const userRoutes = require('./routes/user');
 
 const app = express();
-const port = process.env.PORT || 9000;
+const port = process.env.PORT || 3000;
 
 //middleware
 app.use(express.json());
