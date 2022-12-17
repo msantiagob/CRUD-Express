44555ba6bfcec791e38681d92961cd981594720d
unknown <miguelsant2001@gmail.com>
2022-12-17 12:24:04 -0500

    fix mongoose

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ app.get('/', (req, res) => {
 });
 
 // mongodb connection
-mongoose.set('strictQuery', false);
+mongoose.set('strictQuery', true);
 mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => console.log('Connected to MongoDB Atlas'))
